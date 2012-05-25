3246529e2477ca3576579ce62db0e925294689ea
RainoXu <tblonggang@gmail.com>
2012-05-25 20:02:11 +0800

    debug proxy, add debugger code

--- tool/inject-proxy.php
+++ tool/inject-proxy.php
@@ -2,10 +2,11 @@
     //$queryString = $_SERVER['QUERY_STRING'];
     //$sourceURI = $_SERVER['HTTP_HOST'] . $_SERVER["SCRIPT_NAME"]; 
 
-    //echo('post<br>');
-    //var_dump($_POST);
-    //echo('get<br>');
-    //var_dump($_GET);
+    echo('welcome to proxy<br>');
+    echo('post<br>');
+    var_dump($_POST);
+    echo('get<br>');
+    var_dump($_GET);
 
     $isGetByCurl = $_REQUEST['__TEST__'];
 
@@ -44,5 +45,5 @@
         $output = str_replace('</body>', createInjectList($injectList) . 'insert</body>', $output);
     }
 
-    echo($output);
+    //echo($output);
 ?>
