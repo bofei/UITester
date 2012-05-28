cdfb02d4c85f253fadfdbe6e955ab6ae8d2018dc
RainoXu <tblonggang@gmail.com>
2012-05-28 11:16:33 +0800

    rename index.php to list.php

---
+++ tool/list.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+	<meta charset="gbk">
+	<title>测试用例列表</title>
+</head>
+<body>
+	
+    <div class="case-container">
+        <a href="">添加用例</a>
+
+        <ul>
+        	<li>
+        		<a href="apply.php?case_uri=&target_uri=">第一个测试用例，用于调用代理服务器工作是否正常</a>
+        	</li>
+        </ul>
+    </div>
+</body>
+</html>
